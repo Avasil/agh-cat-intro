b3bc2ba83d16bc66fe31289f9302e1b90085be98
Piotr Gawryś <pgawrys2@gmail.com>
2018-12-04 21:40:59 +0100

    Add recursion exercise

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( test
     ) where
 
+-- Recursion Example
+
+data Tree a = Empty | Node (Tree a) a (Tree a)
+
+height :: Tree a -> Int
+height Empty = 0
+height (Node left _ right) = 1 + max (height left) (height right)
+
+-- Exercise 1: 
+-- Implement recursive fibonacci function three times
+-- using different type of pattern matching each time
+
 test :: IO ()
 test = putStrLn "Hello, SMaDA!"
