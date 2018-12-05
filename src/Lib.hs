b1ae1cf2fc0ee47701ccc0a4fd6873e3c91fb7d1
Piotr Gawryś <pgawrys2@gmail.com>
2018-12-05 07:39:54 +0100

    add fmap exercise

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,5 +14,15 @@ height (Node left _ right) = 1 + max (height left) (height right)
 -- Implement recursive fibonacci function three times
 -- using different type of pattern matching each time
 
+
+-- Exercise 2:
+-- (1) Implement a function that multiplies each element of the list if it is even
+-- (2) Implement your own version of fmap. Hint: Generalize your first function
+-- (3) Implement (1) in terms of (2)
+
+-- Should work like this:
+-- multIfEven [1, 2, 3, 4]
+-- [1, 4, 3, 8]
+
 test :: IO ()
 test = putStrLn "Hello, SMaDA!"
