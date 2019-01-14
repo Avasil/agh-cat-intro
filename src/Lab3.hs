b8722f3da128605eb5b19ca8f3ab83c4cffab016
Piotr Gawryś <pgawrys2@gmail.com>
2019-01-14 20:13:42 +0100

    Lab3 solutions

--- src/Lab3.hs
+++ src/Lab3.hs
@@ -33,79 +33,80 @@ associativity a b c = ((a <> b) <> c) == (a <> (b <> c))
 -- right identity: a <> mempty == a
 
 leftIdentity :: (Eq a, Monoid a) => a -> Bool
-leftIdentity a = undefined
+leftIdentity a = mempty <> a == a
 
 rightIdentity :: (Eq a, Monoid a) => a -> Bool
-rightIdentity a = undefined
+rightIdentity a = a <> mempty == a
 
 -- Bounded Join-Semilattice
 class Monoid m => Semilattice m where
-    join :: m -> m -> m
-    join = (<>)
+   join :: m -> m -> m
+   join = (<>)
 
-    minBound :: m
-    minBound = mempty
+   minBound :: m
+   minBound = mempty
 
 -- commutativity:  a `join` b == b `join` a
 -- idempotence:    a `join` a == a
 
 commutativity :: (Eq a, Semilattice a) => a -> a -> Bool
-commutativity a b = undefined
+commutativity a b = a `join` b == b `join` a
 
 idempotency :: (Eq a, Semilattice a) => a -> Bool
-idempotency a = undefined
+idempotency a = a `join` a == a
+
 
 -- functions on IntMap: 
 -- http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-IntMap-Strict.html
 
-newtype GCounter = GCounter (IntMap Int) 
-    deriving (Arbitrary, Eq, Show)
+newtype GCounter = GCounter (IntMap Int)
+   deriving (Arbitrary, Eq, Show)
 
 -- to increment the counter we need to increment counter on given replica's id
 increment :: Int -> GCounter -> GCounter
-increment id (GCounter imap) = undefined
+increment id (GCounter imap) = GCounter (IntMap.insertWith (+) id 1 imap)
 
 initial :: GCounter
-initial = undefined
+initial = GCounter IntMap.empty
 
 -- to merge we need to take max value of each node
 merge :: GCounter -> GCounter -> GCounter
-merge (GCounter x) (GCounter y) = undefined
+merge (GCounter x) (GCounter y) = GCounter $ IntMap.unionWith max x y
 
 -- value of the total counter is the sum of all nodes
 value :: GCounter -> Int
-value (GCounter imap) = undefined
+value (GCounter imap) = sum imap
 
-splitBrain = do 
-        let initialCounter = GCounter (fromList [(1, 2), (2, 4)])
-        let replica1       = increment 1 initialCounter
-        let replica2       = increment 2 initialCounter
-        print $ value replica1 -- 7
-        print $ value replica2 -- 7
-        print $ value $ merge replica1 replica2 -- 8
+splitBrain = do
+       let initialCounter = GCounter (fromList [(1, 2), (2, 4)])
+       let replica1       = increment 1 initialCounter
+       let replica2       = increment 2 initialCounter
+       print $ value replica1 -- 7
+       print $ value replica2 -- 7
+       print $ value $ merge replica1 replica2 -- 8
 
 -- We defined laws in terms of type classes, so now we need to implement instances:
 -- Hint: you already implemented these functions. :)
 instance Semigroup GCounter where
-    (<>) = undefined
+   (<>) = merge
 
 instance Monoid GCounter where
-    mempty = undefined
+   mempty = initial
 
 instance Semilattice GCounter where
 
 -- Laws take care of most of our properties but it doesn't include incrementing
 -- Try writing your own law for it:
 incrementLaw :: GCounter -> Int -> Bool
-incrementLaw c id = undefined
-    
+incrementLaw c id = value (increment id c) == succ (value c)
+  
 type G = GCounter
-    
+  
 checkAll ::  IO ()
 checkAll = do
-    test "associativity"  (associativity :: G -> G -> G -> Bool)
-    test "left identity"  (leftIdentity  :: G -> Bool)
-    test "right identity" (rightIdentity :: G -> Bool)
-    test "commutativity"  (commutativity :: G -> G -> Bool)
-    test "idempotency"    (idempotency   :: G -> Bool)
-    test "increment"      incrementLaw
+   test "associativity"  (associativity :: G -> G -> G -> Bool)
+   test "left identity"  (leftIdentity  :: G -> Bool)
+   test "right identity" (rightIdentity :: G -> Bool)
+   test "commutativity"  (commutativity :: G -> G -> Bool)
+   test "idempotency"    (idempotency   :: G -> Bool)
+   test "increment"      incrementLaw
